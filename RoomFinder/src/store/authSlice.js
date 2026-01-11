02264982f061098d7bb5642f8683d2ba6082ca61
Sarthak Tomar <sarthaktomar345@gmail.com>
2026-01-11 20:43:13 +0530

    feat: implement authentication slice and store configuration

---
+++ RoomFinder/src/store/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    status: false,
+    userData: null,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers:{
+        login(state,action){
+            state.status = true;
+            state.userData = action.payload;
+        },
+        logout(state){
+            state.userData = null
+            state.status = false
+        }
+    }
+})
+export const {login,logout} = authSlice.actions;
+
+export default authSlice.reducer;
